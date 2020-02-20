2c9d9502d7c3486eb531113d500a0f511c65e839
Edward <Edward@DESKTOP-09V4OT2>
2020-02-20 00:19:28 +0000

    test

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        private static TcpClient tcpClient = null;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("192.168.1.46");
+
+                TcpListener tcpListener = new TcpListener(ipAd, 8001);
+
+                tcpListener.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  tcpListener.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                tcpClient = tcpListener.AcceptTcpClient();
+
+                ReceiveData();
+                tcpClient.Close();
+                tcpListener.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+
+            Console.ReadKey();
+        }
+
+        private async static void ReceiveData()
+        {
+
+            NetworkStream networkStream = null;
+
+            networkStream = tcpClient.GetStream();
+            ASCIIEncoding asen = new ASCIIEncoding();
+
+            byte[] data = new byte[tcpClient.ReceiveBufferSize];
+            MemoryStream memoryStream = new MemoryStream();
+            do
+            {
+                networkStream.Read(data, 0, data.Length);
+                memoryStream.Write(data, 0, data.Length);
+            } while (networkStream.DataAvailable);
+
+            Console.WriteLine("\nFinished Writing");
+            //networkStream.Read(data, 0, tcpClient.ReceiveBufferSize);
+
+            string fileName = @"C:\Users\Edward\Desktop\alfie2.jpg";
+            File.WriteAllBytes(fileName, memoryStream.ToArray());
+
+            byte[] response = asen.GetBytes("Hello client"); ;
+            networkStream.Write(response, 0, response.Length);
+
+            //s.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
+
+            networkStream.Close();
+        }
+    }
+}
